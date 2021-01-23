f615f99ab61f9b3c6e0d1b04da9634a706223a78
IzzieKentie <39170288+IzzieKentie@users.noreply.github.com>
2021-01-23 17:36:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const http = require('http');
 const fs = require('fs');
-const dbConnection = require('public/db.js');
+
+const mysql=require('mysql2');
+ const conn=mysql.createConnection({
+   host:'asefypapplication.mysql.database.azure.com',
+   user:'izziefyp',
+   password:'012414@Kent',
+   database:'ase_application'
+ }).promise();
+module.exports = connection; 
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
