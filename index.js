6beb574a2bdcb913c634f2631de98075aef2aea5
IzzieKentie <39170288+IzzieKentie@users.noreply.github.com>
2021-01-23 10:11:56 +0000

    home

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log("Server running at http://localhost:%d", port); */
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+  fs.readFile('home.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
