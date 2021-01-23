34861354c23beaa66188b6e01c8da171b47ad662
IzzieKentie <39170288+IzzieKentie@users.noreply.github.com>
2021-01-23 18:01:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const http = require('http');
 const fs = require('fs');
 const dbConnection = require('./db');
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
+
+const app = express();
+app.use(express.urlencoded({extended:false}));
+
+app.set('views', path.join(__dirname,'public'));
+app.set('view engine','ejs');
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+    maxAge:  3600 * 1000 // 1hr
+}));
+
+const ifNotLoggedin = (req, res, next) => {
+    if(!req.session.isLoggedIn){
+        return res.render('login');
+    }
+    next();
+}
+
+const ifLoggedin = (req,res,next) => {
+    if(req.session.isLoggedIn){
+        return res.redirect('/home');
+    }
+    next();
+}
+
+app.get('/', ifNotLoggedin, (req,res,next) => {
+    dbConnection.execute("SELECT `username` FROM `ase_team` WHERE `id`=?",[req.session.userID])
+    .then(([rows]) => {
+        res.render('home',{
+            name:rows[0].name
+        });
+    });
+    
+});
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
