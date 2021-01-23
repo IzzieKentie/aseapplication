d03ad71ccd03e4edd6a18c12ea31db206527134c
IzzieKentie <39170288+IzzieKentie@users.noreply.github.com>
2021-01-23 10:18:53 +0000

    home

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const server = http.createServer((request, response) => {
 });
 
 const port = process.env.PORT || 1337;
+server.listen(port); */
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  fs.readFile('home.html', function(err, data) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(data);
+    response.end();
+    return res.end();
+  }
+});
+
+const port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port); */
-
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('home.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+console.log("Server running at http://localhost:%d", port); 
 
