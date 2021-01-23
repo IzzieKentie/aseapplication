99536425f529b23f7ef5f800940ee989f03035e9
IzzieKentie <39170288+IzzieKentie@users.noreply.github.com>
2021-01-23 18:25:48 +0000

    Create db.js

---
+++ public/db.js
@@ -0,0 +1,7 @@
+const mysql=require('mysql2');
+ const conn=mysql.createConnection({
+   host:"asefypapplication.mysql.database.azure.com",
+   user:"izziefyp@asefypapplication",
+   password:"012414@Kent"
+ }).promise();
+module.exports = dbConnection;
